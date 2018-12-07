de2f42c30b9f9898fcf52bfdcf97ecab44bcd482
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2018-12-07 07:29:10 +0330

    Add minor changes

--- index.js
+++ index.js
@@ -125,70 +125,34 @@ bot.use(session())
 bot.use(stage.middleware())
 
 /** -------------------- on START --------------- **/
+
+var username = '';
+
 bot.start((ctx) => {
+  // Get user's username
+  username = ctx.from.username;
+  // Greetings
   ctx.reply(`
-    با سلام ${ctx.from.username}.
+    با سلام ${username}.
     ممنونیم که وقت خودتون رو در اختیار ما گذاشته و به جمع‌آوری دیتاستی از دستورات فارسی کمک می‌کنید 🙏. در ادامه پس از انتخاب گزینه شروع، در هر مرتبه یک دستور به شما نمایش داده می‌شود و از شما خواسته می‌شود که آن را سه بار ضبط کرده و ارسال کنید.
     برای شروع گزینه زیر را نتخاب کنید:
     `,
     Markup.inlineKeyboard([
-      Markup.callbackButton('شروع', 'start_confirmed')
+      Markup.callbackButton('شروع', 'start_confirmed') // Adds a glassy button to start the process
     ]).extra());
   }
 );
 
+// When the شروع glassy button is pressed
 bot.action('start_confirmed', (ctx, next) => {
   ctx.scene.enter('choose_command')
 });
 
-
-
-
-
-
-
-
-// bot.action('next_command', (ctx, next) => {
-//   return showNextMessage(ctx);
-// });
-
-
-// function showMessage(ctx) {
-//   ctx.replyWithHTML(`
-//     لطفا دستور «<b>${commands[session.commandCounter]}</b>» را تلفظ کنید:
-//     `);
-  
-//   return ctx.reply(`مرتبه ${session.commandStatuses[commands[session.commandCounter]].voiceCount}`)
-// }
-
-// function showNextMessage(ctx){
-//   session.commandCounter = session.commandCounter + 1;
-//   showMessage(ctx)
-// }
-
-// bot.on('voice', (ctx) => {
-//   console.log(ctx.message.voice.file_id)
-//   url = bot.telegram.getFileLink(ctx.message.voice.file_id).then(url=>{
-//     console.log(url)
-//     getFile(url);
-//     ctx.reply(url)
-//   })
-// });
-
-
-// bot.command('caption', (ctx) => ctx.replyWithPhoto('https://picsum.photos/200/300/?random', {
-//   caption: 'Caption *text*',
-//   parse_mode: 'Markdown'
-// }));
-
-
-// bot.use((ctx) => {
-//   cmd = ctx.message.text
-//   ctx.reply(`مرتبه ${++session.commandStatuses[cmd].voiceCount}`)
-// });
-
-
-// bot.on('text', (ctx)=>ctx.reply("wow222!"));
+// url = bot.telegram.getFileLink(ctx.message.voice.file_id).then(url=>{
+//       console.log(url)
+//       getFile(url);
+//       ctx.reply(url)
+//     })
 
 
 // function getFile(url){
@@ -198,117 +162,3 @@ bot.action('start_confirmed', (ctx, next) => {
 //     response.pipe(file);
 //   });
 // }
-
-// bot.use(Telegraf.log())
-
-// bot.command('onetime', ({ reply }) =>
-//   reply('One time keyboard', Markup
-//     .keyboard(['/simple', '/inline', '/pyramid'])
-//     .oneTime()
-//     .resize()
-//     .extra()
-//   )
-// )
-
-// bot.command('custom', ({ reply }) => {
-//   return reply('Custom buttons keyboard', Markup
-//     .keyboard([
-//       ['🔍 Search', '😎 Popular'], // Row1 with 2 buttons
-//       ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-//       ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 buttons
-//     ])
-//     .oneTime()
-//     .resize()
-//     .extra()
-//   )
-// })
-
-// bot.hears('🔍 Search', ctx => ctx.reply('Yay!'))
-// bot.hears('📢 Ads', ctx => ctx.reply('Free hugs. Call now!'))
-
-// bot.command('special', (ctx) => {
-//   return ctx.reply('Special buttons keyboard', Extra.markup((markup) => {
-//     return markup.resize()
-//       .keyboard([
-//         markup.contactRequestButton('Send contact'),
-//         markup.locationRequestButton('Send location')
-//       ])
-//   }))
-// })
-
-// bot.command('pyramid', (ctx) => {
-//   return ctx.reply('Keyboard wrap', Extra.markup(
-//     Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
-//       wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2
-//     })
-//   ))
-// })
-
-// bot.command('simple', (ctx) => {
-//   return ctx.replyWithHTML('<b>Coke</b> or <i>Pepsi?</i>', Extra.markup(
-//     Markup.keyboard(['Coke', 'Pepsi'])
-//   ))
-// })
-
-// bot.command('inline', (ctx) => {
-//   return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
-//     m.inlineKeyboard([
-//       m.callbackButton('Coke', 'Coke'),
-//       m.callbackButton('Pepsi', 'Pepsi')
-//     ])))
-// })
-
-// bot.command('random', (ctx) => {
-//   return ctx.reply('random example',
-//     Markup.inlineKeyboard([
-//       Markup.callbackButton('Coke', 'Coke'),
-//       Markup.callbackButton('Dr Pepper', 'Dr Pepper', Math.random() > 0.5),
-//       Markup.callbackButton('Pepsi', 'Pepsi')
-//     ]).extra()
-//   )
-// })
-
-// bot.command('caption', (ctx) => {
-//   return ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' },
-//     Extra.load({ caption: 'Caption' })
-//       .markdown()
-//       .markup((m) =>
-//         m.inlineKeyboard([
-//           m.callbackButton('Plain', 'plain'),
-//           m.callbackButton('Italic', 'italic')
-//         ])
-//       )
-//   )
-// })
-
-// bot.hears(/\/wrap (\d+)/, (ctx) => {
-//   return ctx.reply('Keyboard wrap', Extra.markup(
-//     Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
-//       columns: parseInt(ctx.match[1])
-//     })
-//   ))
-// })
-
-// bot.action('Dr Pepper', (ctx, next) => {
-//   return ctx.reply('👍').then(() => next())
-// })
-
-// bot.action('plain', async (ctx) => {
-//   ctx.editMessageCaption('Caption', Markup.inlineKeyboard([
-//     Markup.callbackButton('Plain', 'plain'),
-//     Markup.callbackButton('Italic', 'italic')
-//   ]))
-// })
-
-// bot.action('italic', (ctx) => {
-//   ctx.editMessageCaption('_Caption_', Extra.markdown().markup(Markup.inlineKeyboard([
-//     Markup.callbackButton('Plain', 'plain'),
-//     Markup.callbackButton('* Italic *', 'italic')
-//   ])))
-// })
-
-// bot.action(/.+/, (ctx) => {
-//   return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`)
-// })
-
-// bot.startPolling()
