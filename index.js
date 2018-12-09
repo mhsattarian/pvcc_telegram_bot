03afc9be56009029700b3d416effbccb882cb66b
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2018-12-09 04:08:52 +0330

    Fix Sessions and Add minor features

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 
-require('dotenv').config(); // Reads the .env file and add each line in environment variables
+// Reads the .env file and add each line in environment variables
+require('dotenv').config();
+
+// TODO: Use persianJs to correct the numbers
 
 const Telegraf = require('telegraf'), // Telegram API wrapper
   Extra = require('telegraf/extra'),
   Markup = require('telegraf/markup'),
-  session = require('telegraf/session'),
   Composer = require('telegraf/composer'),
   Stage = require('telegraf/stage'),
+  session = require('telegraf/session'),
   WizardScene = require('telegraf/scenes/wizard'),
   Scene = require('telegraf/scenes/base'),
+  LocalSession = require('telegraf-session-local'),
   { enter } = Stage;
 
 const http = require('http'),
   https = require('https'),
   fs = require('fs');
+
 
 
 // connects to Bot using the Token
@@ -38,27 +43,14 @@ const commands = [
   'پایین',
   'چپ',
   'راست',
-  'کی ساختت؟'
+  'کی ساختت؟',
 ];
 
-// Number of the command to get its voice
-session.commandCounter = 0;
-// Status of each command (whether its spoken and how many times)
-session.commandStatuses = {}
-// Creating the commandStatuses object using commands array
-for (var i=0; i<commands.length; i++) { 
-  session.commandStatuses[commands[i]] = {
-    done: false,
-    voiceCount : 0
-  };
-}
+/** -------------------- on Using --------------- **/
 
-
-bot.telegram.deleteWebhook(); // for making sure no webhook is running
-bot.startPolling(); // Start listening for updates from the bot
-
-
+// [*not using now*] A helper function to clear all messages from this scene (not from user)
 const sceneCleaner = () => async (ctx) => {
+  // Should store messages in message property of scene's state
   ctx.scene.state.messages.forEach(({ message_id: id }) => {
     try {
       ctx.deleteMessage(id)
@@ -69,71 +61,140 @@ const sceneCleaner = () => async (ctx) => {
 }
 
 // Show a Keyboard of Commands and their voice count
-const chooseCommandKeyboard = () => Markup.keyboard(commands.map(
-  (item)=> `${item} (${session.commandStatuses[item].voiceCount} از 3)`))
+const chooseCommandKeyboard = (userSession) => Markup.keyboard(commands.map(
+  (item)=> `${item} (${userSession.commandStatuses[item].voiceCount} از 3)`))
   .oneTime().resize().extra()
 
-// Stores the command choosen to be pronounced (fill in first scene and uses in seconds)
-var choosenCommand = '';
-
-// The choose your command scene
+// FIRST SCENE : CHOSE YOUR COMMAND
 const firstScene = new Scene('choose_command')
   .enter(async (ctx) => {
+    // [*not used now*] for storing messages to be cleared using `sceneCleaner`
     const messages = []
-    messages.push(await ctx.reply('یکی از دستورات را انتخاب کنید:', chooseCommandKeyboard()))
+
+    // show a keyboard to user to choose between commands
+    messages.push(await ctx.reply('یکی از دستورات را انتخاب کنید:', chooseCommandKeyboard(ctx.userSession)))
+    
     // ctx.scene.state.messages = messages
   })
-  .on('text', (ctx)=>{
-    text = ctx.message.text.split(' ')[0]; // Input text not in commands
-    if (!commands.includes(text)) ctx.reply('لطفا یکی از دستورات لیست را انتخاب کنید.');
-    else choosenCommand = text;
-    if (session.commandStatuses[choosenCommand].voiceCount < 3) ctx.scene.enter('get_voices');
+  .on('text', async (ctx)=>{
+    // if (ctx.message.text.startsWith('/')) {
+    //   console.log("Done");
+    //   return ctx.reply("لطفا ابتدا دستور مورد نظر را انتخاب کنید")
+    // }
+
+    // cuase command had become (روشن (0 از 3))
+    command = ctx.message.text.split(' ')[0];
+    
+    // Error if input text not in commands
+    if (!commands.includes(command)) 
+      ctx.reply('لطفا یکی از دستورات لیست را انتخاب کنید.');
+    // Stores the command choosen to be pronounced
+    // (fills in first scene and uses in seconds)
+    else ctx.session.choosenCommand = command;
+    
+    // If a command is spoken *less than* 3 times, go to second scene (pronouncing commands)
+    if (ctx.userSession.commandStatuses[command].voiceCount < 3)
+      ctx.scene.enter('get_voices');
+    // otherwise Error to choose another command
     else ctx.reply('یکی دیگر از دستورات را انتخاب کنید:')
   })
+  // if user tryed to send a recoreded voice in this scene Error
   .on('voice', (ctx)=>ctx.reply("لطفا اول یک دستور را انتخاب کنید"))
-  .leave(async (ctx)=> {})
+  // What to happen when leaving this scene (including switching between scenes)
+  .leave(async (ctx)=> {
+    // ctx.reply("hey",
+    //   {ReplyKeyboardRemove: { remove_keyboard : true }},
+    // );
+  });
 
+
+// SECOND SCENE : PRONOUNCE THE COMMAND
 const secondScene = new Scene('get_voices')
   .enter((ctx) => {
+    // [*not used now*] for storing messages to be cleared using `sceneCleaner`
     const messages = []
-    var voiceCount = ++session.commandStatuses[choosenCommand].voiceCount;
+    
+    // If the command is pronounced 3 times go back to scene one (choosing commands)
+    var voiceCount = ++ctx.userSession.commandStatuses[ctx.session.choosenCommand].voiceCount;
     if (voiceCount > 3) {
-      --session.commandStatuses[choosenCommand].voiceCount;
+      // Cause we added once on line above
+      --ctx.userSession.commandStatuses[ctx.session.choosenCommand].voiceCount;
       ctx.scene.enter('choose_command')
     }
     else{
-      console.log(voiceCount)
+      // Ask user to pronounce the command
       ctx.reply(`
       مرتبه ${voiceCount == 1 ? '1️⃣' : voiceCount == 2 ? '2️⃣' : '3️⃣'}
       صدای خود را ضبط کرده و ارسال کنید:
       `)
     }
+    
     // ctx.scene.state.messages = messages
   })
   .on('voice', (ctx)=>{
+    // Thank the user
     ctx.reply("👌");
-    console.log(session.commandStatuses[choosenCommand].voiceCount);
+    // ReEnter in current scene to ask for pronounciation again if needed
     ctx.scene.reenter();
   })
+  // if user tryed to type something in this scene Error
+  .on('text', (ctx)=>ctx.reply("لطفا دستور مورد نظر را با صدای خود ضبط کرده و ارسال کنید."))
+  // What to happen when leaving this scene (including switching between scenes)
   .leave(async (ctx) => {
   })
 
 
-
-const stage = new Stage([firstScene, secondScene], { ttl: 10 })
+// Initialize the session (from now on the session object of ctx can be accessed)
+bot.use((new LocalSession({ database: 'sessions.json',  property: 'userSession'})).middleware());
+// Tell the bot to use these stages
 bot.use(session())
+
+// Define Bot stages (scenes)
+const stage = new Stage([firstScene, secondScene], { ttl: 10 })
 bot.use(stage.middleware())
+
+
+// for making sure no webhook is running
+bot.telegram.deleteWebhook(); 
+// Start listening for updates from the bot
+bot.startPolling();
+
+
+getSessionKey = (ctx) => {
+  if (ctx.from && ctx.chat) {
+    return `${ctx.from.id}:${ctx.chat.id}`
+  } 
+  return null
+}
 
 /** -------------------- on START --------------- **/
 
-var username = '';
+// TODO : chack if user has aleardy done some commands, dont clear it's history
 
+// 1. Initializations and greetings
 bot.start((ctx) => {
-  // Get user's username
-  username = ctx.from.username;
+  // choose active user's session from the session object
+  var userSession = ctx.userSession;
+  // Number of the command to get its voice
+  userSession.commandCounter = 0;
+  // Status of each command (whether its spoken and how many times)
+  userSession.commandStatuses = {}
+  // Creating the commandStatuses object using commands array
+  commands.map(command=>{
+    userSession.commandStatuses[command] = {
+      done: false,
+      voiceCount : 0
+    }
+  })
+  // Save user's username , name
+  userSession.userName = ctx.from.username;
+  userSession.fullName = ctx.from.first_name + ctx.from.last_name;
+  // Add a random number to chack the userSession working (TODO: remove this)
+  userSession.random = Math.random();
+
   // Greetings
   ctx.reply(`
-    با سلام ${username}.
+    با سلام ${userSession.userName || userSession.fullName}.
     ممنونیم که وقت خودتون رو در اختیار ما گذاشته و به جمع‌آوری دیتاستی از دستورات فارسی کمک می‌کنید 🙏. در ادامه پس از انتخاب گزینه شروع، در هر مرتبه یک دستور به شما نمایش داده می‌شود و از شما خواسته می‌شود که آن را سه بار ضبط کرده و ارسال کنید.
     برای شروع گزینه زیر را نتخاب کنید:
     `,
@@ -143,10 +204,27 @@ bot.start((ctx) => {
   }
 );
 
-// When the شروع glassy button is pressed
+// 2. When the شروع glassy button is pressed
 bot.action('start_confirmed', (ctx, next) => {
   ctx.scene.enter('choose_command')
 });
+
+// When the شروع glassy button is pressed
+bot.command('session', ctx => {
+  ctx.reply('session: \n' + JSON.stringify(ctx.session));
+  ctx.reply('UserSession: \n' + JSON.stringify(ctx.userSession));
+});
+
+// TODO: make cancel button work
+
+// 2. When the شروع glassy button is pressed
+bot.hears('[لغو]', ctx => {
+  // Remove the keyboard if exist!
+  ctx.reply(undefined,
+    Extra.markup(Markup.removeKeyboard()),
+  );
+});
+
 
 // url = bot.telegram.getFileLink(ctx.message.voice.file_id).then(url=>{
 //       console.log(url)
